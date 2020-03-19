26b74ec5adc5d86ff3e7b648b490047533cf08f0
Eric Flumerfelt <eflumerf@fnal.gov>
2020-03-19 11:51:53 -0500

    Add some basic CMake commands to top-level CMakeLists.txt. Fill in basic functionality in DAQProcess_test so that it actually builds in environments where Boost is available

--- unittest/DAQProcess_test.cc
+++ unittest/DAQProcess_test.cc
@@ -1,1 +1,20 @@
 #include "app-framework/DAQProcess.hh"
+#include "app-framework/QueryResponseCommandFacility.hh"
+
+
+namespace appframework {
+std::unique_ptr<CommandFacility> CommandFacility::handle_ = std::unique_ptr<CommandFacility>(new QueryResponseCommandFacility());
+}
+
+#define BOOST_TEST_MODULE DAQProcess_test
+
+#include <boost/test/auto_unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(DAQProcess_test)
+
+BOOST_AUTO_TEST_CASE(Construct) {
+    std::list<std::string> args;
+    appframework::DAQProcess dp(args);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
