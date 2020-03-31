54d894c7e9b220fb7f0a14a36afbcb6a6d4d00ee
Wesley Ketchum <wketchum@fnal.gov>
2020-03-31 10:13:23 -0500

    initial stab at base Buffer class

--- include/app-framework-base/Buffers/Buffer.hh
+++ include/app-framework-base/Buffers/Buffer.hh
@@ -1,8 +1,37 @@
 #ifndef app_framework_base_Buffers_Buffer_hh
 #define app_framework_base_Buffers_Buffer_hh
 
-namespace appframework {
-class Buffer {};
-}  // namespace appframework
+#include <cstddef>
+using std::size_t;
+
+namespace appframework{
+
+  template <class T>
+  class Buffer {
+    
+  public:
+
+    void Configure() = 0; ///called when configured in FSM
+    
+    bool isBounded() { return fBounded; }
+
+    virtual size_t size() = 0; ///elements in buffer
+    size_t sizeBytes() { return size()*sizeof(T); }
+    
+    virtual size_t capacity() = 0;
+    size_t capacityBytes() { return capacity()*sizeof(T); }
+
+    //basic element adding functions
+    virtual size_t push(const T&) = 0; ///push one on
+    virtual size_t pop(T&) = 0; ///pop one off
+
+
+  protected:
+    
+    bool fBounded;  ///can size increase dynamically?
+
+  };
+
+}
 
 #endif  // app_framework_base_Buffers_Buffer_hh
