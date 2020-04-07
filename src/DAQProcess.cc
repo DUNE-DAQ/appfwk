0337fbc1493f4ad5c1286b972c867c7d9e89523b
Eric Flumerfelt <eflumerf@fnal.gov>
2020-04-07 10:21:03 -0500

    Revise DAQProcess::execute_command to make sure that UserModules not in the CommandOrderMap for a given command still receive that command

--- src/DAQProcess.cc
+++ src/DAQProcess.cc
@@ -12,6 +12,11 @@
 #include "app-framework-base/Services/Logger.hh"
 #include "app-framework-base/Services/ServiceManager.hh"
 
+#include "TRACE/trace.h"
+#define TRACE_NAME "DAQProcess"
+
+#include <unordered_set>
+
 namespace appframework {
 ServiceManager* ServiceManager::handle_ = nullptr;
 ConfigurationManager* ConfigurationManager::handle_ = nullptr;
@@ -23,21 +28,30 @@ DAQProcess::DAQProcess(std::list<std::string> args) {
     ServiceManager::setup(args);
 }
 
-void DAQProcess::register_modules(std::unique_ptr<ModuleList> const& ml) {
-    ml->ConstructGraph(bufferMap_, userModuleMap_, commandOrderMap_);
-}
+void DAQProcess::register_modules(std::unique_ptr<ModuleList> const& ml) { ml->ConstructGraph(bufferMap_, userModuleMap_, commandOrderMap_); }
 
 void DAQProcess::execute_command(std::string cmd) {
+    std::unordered_set<std::string> user_module_list;
+    for (auto const& um : userModuleMap_) {
+        user_module_list.insert(um.first);
+    }
+
+    TLOG(TLVL_DEBUG) << "Executing Command " << cmd << " for UserModules defined in the CommandOrderMap";
     if (commandOrderMap_.count(cmd)) {
         for (auto& moduleName : commandOrderMap_[cmd]) {
             if (userModuleMap_.count(moduleName)) {
                 userModuleMap_[moduleName]->execute_command(cmd);
+                user_module_list.erase(moduleName);
             }
         }
     } else {
-        for (auto const& um : userModuleMap_) {
-            um.second->execute_command(cmd);
-        }
+        TLOG(TLVL_WARNING) << "Command " << cmd
+                           << " does not have an entry in the CommandOrderMap! UserModules will receive this command in an unspecified order!";
+    }
+
+    TLOG(TLVL_DEBUG) << "Executing Command " << cmd << " for all remaining UserModules";
+    for (auto const& moduleName : user_module_list) {
+        userModuleMap_[moduleName]->execute_command(cmd);
     }
 }
 
