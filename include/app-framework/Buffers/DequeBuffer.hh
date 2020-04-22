9bda527f0ccb1f4c5aed07faebd03dbf6d9afc35
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-04-21 19:10:32 -0500

    JCF: move the implementation of DequeBuffer's functions into the DequeBuffer's header

--- include/app-framework/Buffers/DequeBuffer.hh
+++ include/app-framework/Buffers/DequeBuffer.hh
@@ -47,6 +47,75 @@ namespace appframework{
     size_t fPopRetries;
   };
 
+
+template <class T>
+DequeBuffer<T>::DequeBuffer(){
+  Configure();
+}
+
+template <class T>
+void DequeBuffer<T>::Configure(){
+  this->fAttributes.isBounded = true;
+  this->fAttributes.isSearchable = false;
+  
+  fDeque = std::deque<T>();
+  fSize = 0 ;
+
+  if(this->attributes().isBounded){
+    fCapacity = 1000;
+    } else {
+    fCapacity = fDeque.max_size();
+  }
+
+  this->fPushTimeout_ms = 1e6;
+  this->fPopTimeout_ms  = 1e7;
+  fRetryTime_ms         = 100;
+
+  fPushRetries = this->fPushTimeout_ms/fRetryTime_ms;
+  fPopRetries  = this->fPopTimeout_ms/fRetryTime_ms;
+}
+
+template <class T>
+int DequeBuffer<T>::push(T&& obj) {
+  int n_retries=0;
+  while(full()){
+    if(n_retries==fPushRetries){
+      //could throw, but here choose to return -1
+      //throw std::runtime_error("Reached Max Push Retries in DequeBuffer");
+      return -1;
+    }
+    ++n_retries;
+    usleep(fRetryTime_ms);
+  }
+  
+  if(full()) throw std::runtime_error("Full after check? Another thread accessing?");
+  std::lock_guard<std::mutex> lck(fMutex);
+    fDeque.emplace_back(obj);
+  fSize ++ ;
+  return size();
+}
+
+template <class T>
+T DequeBuffer<T>::pop(){
+  int n_retries=0;
+  while(empty()){
+    if(n_retries==fPopRetries){
+      throw std::runtime_error("No Data: Reached Max Pop Retries in DequeBuffer");
+      //return -1;
+    }
+    ++n_retries;
+    usleep(fRetryTime_ms);
+  }
+
+  if(empty()) throw std::runtime_error("Empty after check? Another thread accessing?");
+
+  std::lock_guard<std::mutex> lck(fMutex);
+  T obj(std::move(fDeque.front()));
+  fDeque.pop_front();
+    fSize--;
+  return obj;
+}
+
 }  // namespace appframework
 
 #endif // APP_FRAMEWORK_INCLUDE_APP_FRAMEWORK_BUFFERS_DEQUEBUFFER_HH_
