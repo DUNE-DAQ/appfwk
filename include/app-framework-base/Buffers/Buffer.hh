faf18924c9ae7bb0356a2deafd8e4bf0ba09a65c
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-04-22 18:01:33 -0500

    JCF: clang-format the buffer files using Pengfei's suggested file at https://hackmd.io/k7xo2AhSRiy_rribpXp97g

--- include/app-framework-base/Buffers/Buffer.hh
+++ include/app-framework-base/Buffers/Buffer.hh
@@ -2,12 +2,13 @@
 #define APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_BUFFERS_BUFFER_HH_
 
 /**
- * @file Buffer class interface which augments BufferI with push and pop functions
+ * @file Buffer class interface which augments BufferI with push and pop
+ * functions
  *
  * This is part of the DUNE DAQ Application Framework, copyright 2020.
- * Licensing/copyright details are in the COPYING file that you should have received with this code.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
  */
-
 
 #include "BufferI.hh"
 
@@ -15,69 +16,55 @@
 #include <utility>
 using std::size_t;
 
-namespace appframework{
+namespace appframework {
 
-  template <class T>
-  class Buffer : public BufferI {
-    
-  public:
+template <class T> class Buffer : public BufferI {
 
-    virtual size_t capacityBytes() const noexcept {return this->capacity()*sizeof(T);} ///bytes in buffer
+public:
+  virtual size_t capacityBytes() const noexcept {
+    return this->capacity() * sizeof(T);
+  } /// bytes in buffer
+};
 
-  };
+template <class T> class BufferInput : virtual public Buffer<T> {
 
+public:
+  explicit BufferInput(size_t pt = 10) : fPushTimeout_ms(pt) {}
 
-  template <class T>
-  class BufferInput : virtual public Buffer<T>{
+  virtual int push(const T &t) { return push(std::move(t)); }
+  virtual int push(T &&) = 0;
 
-  public:
+  size_t get_push_timeout() const { return fPushTimeout_ms; }
+  void set_push_timeout(size_t sz) { fPushTimeout_ms = sz; }
 
-    explicit BufferInput(size_t pt = 10) : 
-      fPushTimeout_ms(pt) 
-    {}
+  BufferInput(const BufferInput &) = default;
+  BufferInput &operator=(const BufferInput &) = default;
+  BufferInput(BufferInput &&) = default;
+  BufferInput &operator=(BufferInput &&) = default;
 
-    virtual int push(const T& t) { return push( std::move(t) ); }
-    virtual int push(T&& ) = 0;
+private:
+  size_t fPushTimeout_ms;
+};
 
-    size_t get_push_timeout() const { return fPushTimeout_ms; }
-    void set_push_timeout(size_t sz) { fPushTimeout_ms = sz; }
+template <class T> class BufferOutput : virtual public Buffer<T> {
 
-    BufferInput(const BufferInput& ) = default;
-    BufferInput& operator=(const BufferInput& ) = default;
-    BufferInput(BufferInput&& ) = default;
-    BufferInput& operator=(BufferInput&& ) = default;
+public:
+  explicit BufferOutput(size_t pt = 10) : fPopTimeout_ms(pt) {}
 
+  virtual T pop() = 0;
 
-  private:
-    size_t  fPushTimeout_ms;
-    
-  };
+  size_t get_pop_timeout() const { return fPopTimeout_ms; }
+  void set_pop_timeout(size_t sz) { fPopTimeout_ms = sz; }
 
-  template <class T>
-  class BufferOutput : virtual public Buffer<T>{
+  BufferOutput(const BufferOutput &) = default;
+  BufferOutput &operator=(const BufferOutput &) = default;
+  BufferOutput(BufferOutput &&) = default;
+  BufferOutput &operator=(BufferOutput &&) = default;
 
-  public:
+private:
+  size_t fPopTimeout_ms;
+};
 
-    explicit BufferOutput(size_t pt = 10) :
-         fPopTimeout_ms(pt) 
-    {}
-
-    virtual T pop() = 0;
-
-    size_t get_pop_timeout() const { return fPopTimeout_ms; }
-    void set_pop_timeout(size_t sz) { fPopTimeout_ms = sz; }
-
-    BufferOutput(const BufferOutput& ) = default;
-    BufferOutput& operator=(const BufferOutput& ) = default;
-    BufferOutput(BufferOutput&& ) = default;
-    BufferOutput& operator=(BufferOutput&& ) = default;
-
-
-  private:
-    size_t  fPopTimeout_ms;
-  };
-
-
-} // namespace appframework  
+} // namespace appframework
 
 #endif // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_BUFFERS_BUFFER_HH_
