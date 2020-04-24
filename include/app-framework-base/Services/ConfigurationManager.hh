7ac01530db104f396f33b6b41b8f3618b96bac5d
John Freeman <jcfree@mu2edaq13.fnal.gov>
2020-04-24 12:03:28 -0500

    JCF: clang-format all the files on this branch

--- include/app-framework-base/Services/ConfigurationManager.hh
+++ include/app-framework-base/Services/ConfigurationManager.hh
@@ -2,7 +2,8 @@
  * @file ConfigurationManager service interface definition
  *
  * This is part of the DUNE DAQ Application Framework, copyright 2020.
- * Licensing/copyright details are in the COPYING file that you should have received with this code.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
  */
 
 #ifndef APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_CONFIGURATIONMANAGER_HH_
@@ -13,30 +14,35 @@
 
 namespace appframework {
 /**
- * @brief The ConfigurationManager is responsible for receiving configurations from CCM and making them available to UserModules
+ * @brief The ConfigurationManager is responsible for receiving configurations
+ * from CCM and making them available to UserModules
  */
 class ConfigurationManager {
-   public:
-    /**
-     * @brief Singleton pattern, get a handle to the ConfigurationManager
-     * @return Reference to the ConfigurationManager
-     */
-    static ConfigurationManager& handle() {
-        if (!handle_) handle_.reset( new ConfigurationManager() );
-        return *handle_;
-    }
-    /**
-     * @brief Perform basic setup actions needed by ConfigurationManager, using the provided command-line paramters
-     * @param args Command-line arguments used to setup ConfigurationManager
-     */
-    static void setup(std::list<std::string> /*args*/) {}
+public:
+  /**
+   * @brief Singleton pattern, get a handle to the ConfigurationManager
+   * @return Reference to the ConfigurationManager
+   */
+  static ConfigurationManager &handle() {
+    if (!handle_)
+      handle_.reset(new ConfigurationManager());
+    return *handle_;
+  }
+  /**
+   * @brief Perform basic setup actions needed by ConfigurationManager, using
+   * the provided command-line paramters
+   * @param args Command-line arguments used to setup ConfigurationManager
+   */
+  static void setup(std::list<std::string> /*args*/) {}
 
-   protected:
-    ConfigurationManager() {} ///< Singleton pattern, protected parameterless constructor
+protected:
+  ConfigurationManager() {
+  } ///< Singleton pattern, protected parameterless constructor
 
-   private:
-     static std::unique_ptr<ConfigurationManager> handle_; ///< Singleton pattern, static handle_ member
+private:
+  static std::unique_ptr<ConfigurationManager>
+      handle_; ///< Singleton pattern, static handle_ member
 };
-}  // namespace appframework
+} // namespace appframework
 
-#endif  // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_CONFIGURATIONMANAGER_HH_
+#endif // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_CONFIGURATIONMANAGER_HH_
--- include/app-framework-base/Services/Service.hh
+++ include/app-framework-base/Services/Service.hh
@@ -2,7 +2,8 @@
  * @file Service interface definition
  *
  * This is part of the DUNE DAQ Application Framework, copyright 2020.
- * Licensing/copyright details are in the COPYING file that you should have received with this code.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
  */
 
 #ifndef APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_SERVICE_HH_
@@ -13,17 +14,17 @@
 
 namespace appframework {
 /**
- * @brief The Service class represents a static entity which may be loaded into the ServiceManager for
- * use by multiple UserModules and/or Buffers
+ * @brief The Service class represents a static entity which may be loaded into
+ * the ServiceManager for use by multiple UserModules and/or Buffers
  */
 class Service {
-   public:
-    /**
-     * @brief Configure the Service instance with command-line-like arguments
-     * @param args Arguments used to configure the Service
-     */
-    virtual void setup(std::list<std::string> args) = 0;
+public:
+  /**
+   * @brief Configure the Service instance with command-line-like arguments
+   * @param args Arguments used to configure the Service
+   */
+  virtual void setup(std::list<std::string> args) = 0;
 };
-}  // namespace appframework
+} // namespace appframework
 
-#endif  // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_SERVICE_HH_
+#endif // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_SERVICES_SERVICE_HH_
