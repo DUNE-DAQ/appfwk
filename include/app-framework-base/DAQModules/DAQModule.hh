b0e199581f4cd5454651ff5d704a599d63e0b297
Marco Roda <mroda@liverpool.ac.uk>
2020-05-12 16:48:34 +0100

    Change UserModule to DAQModule

--- include/app-framework-base/DAQModules/DAQModule.hh
+++ include/app-framework-base/DAQModules/DAQModule.hh
@@ -1,10 +1,10 @@
 /**
- * @file UserModule Class Interface
+ * @file DAQModule Class Interface
  *
- * The UserModule interface defines the required functionality for all
- * UserModules that use the Application Framework. UserModules are defined as "a
+ * The DAQModule interface defines the required functionality for all
+ * UserModules that use the Application Framework. DAQModules are defined as "a
  * set of code which performs a specific task". They are connected to one
- * another within a DAQ Application by Buffer instances, in a graph defined by a
+ * another within a DAQ Application by Queues instances, in a graph defined by a
  * ModuleList.
  *
  * This interface is intended to define only absolutely necessary methods to be
@@ -16,21 +16,21 @@
  * received with this code.
  */
 
-#ifndef APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_USERMODULES_USERMODULE_HH_
-#define APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_USERMODULES_USERMODULE_HH_
+#ifndef APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_DAQMODULES_DAQMODULE_HH_
+#define APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_DAQMODULES_DAQMODULE_HH_
 
 #include <future>
 #include <string>
 
 namespace appframework {
 /**
- * @brief The UserModule class is a set of code which performs a specific task.
+ * @brief The DAQModule class is a set of code which performs a specific task.
  *
- * This interface defines the basic methods which all UserModules should expose.
- * Developers implementing UserModules should feel free to use whatever Plugins
+ * This interface defines the basic methods which all DAQModules should expose.
+ * Developers implementing DAQModules should feel free to use whatever Plugins
  * and Services are necessary to accomplish their needed functionality.
  */
-class UserModule {
+class DAQModule {
 public:
   /**
    * @brief Execute a command in this UserModule
@@ -43,8 +43,8 @@ public:
    * return their result. Non-accepted commands should return a status
    * indicating this result.
    */
-  virtual std::future<std::string> execute_command(std::string cmd) = 0;
+  virtual void execute_command( const std::string & cmd, const std::vector<std::string> & args = {} ) = 0;
 };
 } // namespace appframework
 
-#endif // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_USERMODULES_USERMODULE_HH_
+#endif // APP_FRAMEWORK_BASE_INCLUDE_APP_FRAMEWORK_BASE_DAQMODULES_DAQMODULE_HH_
