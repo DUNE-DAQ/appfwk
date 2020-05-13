ca70223d221684f9216207290675bc0b826bf7ad
Marco Roda <mroda@liverpool.ac.uk>
2020-05-13 12:45:16 +0100

    Adding exception description

--- include/app-framework-base/DAQModules/DAQModule.hh
+++ include/app-framework-base/DAQModules/DAQModule.hh
@@ -2,13 +2,13 @@
  * @file DAQModule Class Interface
  *
  * The DAQModule interface defines the required functionality for all
- * UserModules that use the Application Framework. DAQModules are defined as "a
+ * DAQModules that use the Application Framework. DAQModules are defined as "a
  * set of code which performs a specific task". They are connected to one
  * another within a DAQ Application by Queues instances, in a graph defined by a
  * ModuleList.
  *
  * This interface is intended to define only absolutely necessary methods to be
- * able to support the many different tasks that UserModules will be asked to
+ * able to support the many different tasks that DAQModules will be asked to
  * perform.
  *
  * This is part of the DUNE DAQ Application Framework, copyright 2020.
@@ -33,14 +33,14 @@ namespace appframework {
 class DAQModule {
 public:
   /**
-   * @brief Execute a command in this UserModule
+   * @brief Execute a command in this DAQModule
    * @param cmd The command from CCM
    * @return String with detailed status of the command (future).
    *
    * execute_command is the single entry point for DAQProcess to pass CCM
-   * commands to UserModules. The implementation of this function should route
-   * accepted commands to the appropriate functions within the UserModule and
-   * return their result. Non-accepted commands should return a status
+   * commands to DAQModules. The implementation of this function should route
+   * accepted commands to the appropriate functions within the DAQModule.
+   *  Non-accepted commands or failure should return an std::exception 
    * indicating this result.
    */
   virtual void execute_command( const std::string & cmd, const std::vector<std::string> & args = {} ) = 0;
