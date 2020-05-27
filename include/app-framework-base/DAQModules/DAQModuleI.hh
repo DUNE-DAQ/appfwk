1ef0eb52500bc5419c1cec822562922b1eea5c42
Eric Flumerfelt <eflumerf@fnal.gov>
2020-05-27 12:29:48 -0500

    Rename to bring into line with develop

--- include/app-framework-base/DAQModules/DAQModuleI.hh
+++ include/app-framework-base/DAQModules/DAQModuleI.hh
@@ -25,8 +25,6 @@
 #include "cetlib/BasicPluginFactory.h"
 #include "cetlib/compiler_macros.h"
 
-#include "app-framework-base/Queues/Queue.hh"
-
 #ifndef EXTERN_C_FUNC_DECLARE_START
 #define EXTERN_C_FUNC_DECLARE_START                                            \
   extern "C"                                                                   \
@@ -35,27 +33,27 @@
 
 #define DEFINE_DUNE_USER_MODULE(klass)                                         \
   EXTERN_C_FUNC_DECLARE_START                                                  \
-  std::unique_ptr<appframework::DAQModule> make(                               \
+  std::unique_ptr<appframework::DAQModuleI> make(                               \
     std::string n,                                                             \
     std::vector<std::shared_ptr<appframework::QueueI>> i,                      \
     std::vector<std::shared_ptr<appframework::QueueI>> o)                      \
   {                                                                            \
-    return std::unique_ptr<appframework::DAQModule>(new klass(n, i, o));       \
+    return std::unique_ptr<appframework::DAQModuleI>(new klass(n, i, o));       \
   }                                                                            \
   }
 
 namespace appframework {
 /**
- * @brief The DAQModule class is a set of code which performs a specific task.
+ * @brief The DAQModuleI class implementations are a set of code which performs a specific task.
  *
  * This interface defines the basic methods which all DAQModules should expose.
  * Developers implementing DAQModules should feel free to use whatever Plugins
  * and Services are necessary to accomplish their needed functionality.
  */
-class DAQModule
+class DAQModuleI
 {
 public:
-  DAQModule(std::string name,
+  DAQModuleI(std::string name,
             std::vector<std::shared_ptr<QueueI>> inputs,
             std::vector<std::shared_ptr<QueueI>> outputs)
     : instance_name_(name)
@@ -64,13 +62,13 @@ public:
   {}
 
   /**
-   * @brief Execute a command in this DAQModule
+   * @brief Execute a command in this DAQModuleI
    * @param cmd The command from CCM
    * @return String with detailed status of the command (future).
    *
    * execute_command is the single entry point for DAQProcess to pass CCM
    * commands to DAQModules. The implementation of this function should route
-   * accepted commands to the appropriate functions within the DAQModule.
+   * accepted commands to the appropriate functions within the DAQModuleI.
    *  Non-accepted commands or failure should return an std::exception
    * indicating this result.
    */
@@ -83,7 +81,7 @@ protected:
   std::vector<std::shared_ptr<QueueI>> outputs_;
 };
 
-inline std::unique_ptr<DAQModule>
+inline std::unique_ptr<DAQModuleI>
 makeModule(std::string const& module_name,
            std::string const& instance_name,
            std::vector<std::shared_ptr<QueueI>> inputs,
@@ -91,7 +89,7 @@ makeModule(std::string const& module_name,
 {
   static cet::BasicPluginFactory bpf("duneDAQModule", "make");
 
-  return bpf.makePlugin<std::unique_ptr<DAQModule>>(
+  return bpf.makePlugin<std::unique_ptr<DAQModuleI>>(
     module_name, instance_name, inputs, outputs);
 }
 } // namespace appframework
