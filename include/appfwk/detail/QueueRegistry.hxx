0a52bd45903274953de72fb35cef37c8a9534f1a
Marco Roda <mroda@liverpool.ac.uk>
2020-06-16 14:32:19 +0100

    Updated QueueRegistry details

--- include/appfwk/detail/QueueRegistry.hxx
+++ include/appfwk/detail/QueueRegistry.hxx
@@ -1,42 +1,44 @@
 #include "appfwk/StdDeQueue.hpp"
+#include "appfwk/FollyQueue.hpp"
 
 #include <cxxabi.h>
 
 // Declarations
 namespace dunedaq::appfwk {
 
-QueueConfig::queue_kind
-QueueConfig::stoqk(const std::string& name)
-{
-  if (name == "StdDeQueue" || name == "std_deque")
-    return queue_kind::kStdDeQueue;
-  else
-    throw QueueKindUnknown(ERS_HERE, name);
-}
-
-template<typename T>
-std::shared_ptr<Queue<T>>
-QueueRegistry::get_queue(const std::string& name)
-{
-
-  auto itQ = queue_registry_.find(name);
-  if (itQ != queue_registry_.end()) {
-    auto queuePtr = std::dynamic_pointer_cast<Queue<T>>(itQ->second.instance);
-
-    if (!queuePtr) {
-      int status;
-      std::string realname_target = abi::__cxa_demangle(typeid(T).name(), 0, 0, &status);
-      std::string realname_source = abi::__cxa_demangle(itQ->second.type->name(), 0, 0, &status);
-
-      throw QueueTypeMismatch(ERS_HERE, name, realname_source, realname_target);
-    }
-
-    return queuePtr;
+  QueueConfig::queue_kind QueueConfig::stoqk( const std::string & name )   {
+    if (name == "StdDeQueue" || name == "std_deque")
+      return queue_kind::kStdDeQueue ;
+    else if (name == "FollySPSCQueue")
+      return queue_kind::kFollySPSCQueue ;
+    else if (name == "FollyMPMCQueue")
+      return queue_kind::kFollyMPMCQueue ;
+    else
+      throw QueueKindUnknown( ERS_HERE, name ) ;
   }
 
+template<typename T>
+std::shared_ptr<Queue<T>> 
+QueueRegistry::get_queue( const std::string & name) {
+
+  auto itQ = queue_registry_.find(name);
+  if ( itQ != queue_registry_.end() ) {
+      auto queuePtr =  std::dynamic_pointer_cast<Queue<T>>(itQ->second.instance);
+
+      if (!queuePtr) {
+        int     status;
+        std::string realname_target = abi::__cxa_demangle(typeid(T).name(), 0, 0, &status);
+        std::string realname_source = abi::__cxa_demangle(itQ->second.type->name(), 0, 0, &status);
+
+        throw QueueTypeMismatch(ERS_HERE, name, realname_source, realname_target);
+      }
+
+      return queuePtr;
+  } 
+
   auto itP = this->queue_configmap_.find(name);
-  if (itP != queue_configmap_.end()) {
-    QueueEntry entry = { &typeid(T), create_queue<T>(name, itP->second) };
+  if ( itP != queue_configmap_.end() ) {
+    QueueEntry entry = {&typeid(T), create_queue<T>(name, itP->second)};
     queue_registry_[name] = entry;
     return std::dynamic_pointer_cast<Queue<T>>(entry.instance);
 
@@ -46,16 +48,23 @@ QueueRegistry::get_queue(const std::string& name)
 }
 
 template<typename T>
-std::shared_ptr<NamedObject>
-QueueRegistry::create_queue(std::string name, const QueueConfig& config)
-{
+std::shared_ptr<NamedObject> QueueRegistry::create_queue(std::string name, const QueueConfig& config) {
 
   std::shared_ptr<NamedObject> queue;
-  switch (config.kind) {
-    case QueueConfig::kStdDeQueue:
+  switch(config.kind) {
+    case QueueConfig::kStdDeQueue :
       queue = std::make_shared<StdDeQueue<T>>(name);
       std::dynamic_pointer_cast<StdDeQueue<T>>(queue)->SetSize(config.size);
       break;
+    case QueueConfig::kFollySPSCQueue :
+      queue = std::make_shared<FollySPSCQueue<T>>(name);
+      std::dynamic_pointer_cast<FollySPSCQueue<T>>(queue)->SetSize(config.size);
+      break;
+    case QueueConfig::kFollyMPMCQueue :
+      queue = std::make_shared<FollyMPMCQueue<T>>(name);
+      std::dynamic_pointer_cast<FollyMPMCQueue<T>>(queue)->SetSize(config.size);
+      break;
+
     default:
       throw std::runtime_error("Unknown queue kind");
   }
@@ -64,3 +73,4 @@ QueueRegistry::create_queue(std::string name, const QueueConfig& config)
 }
 
 } // namespace dunedaq::appfwk
+
