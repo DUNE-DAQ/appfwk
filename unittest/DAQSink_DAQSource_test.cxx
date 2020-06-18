521bffcc32672ce570643616c2065efdaccd839d
Eric Flumerfelt <eflumerf@fnal.gov>
2020-06-18 10:56:44 -0500

    Recast daq_sink_source as unit test.

---
+++ unittest/DAQSink_DAQSource_test.cxx
@@ -0,0 +1,104 @@
+/**
+ * @file DAQSink_DAQSource_test.cxx Test application which demonstrates the
+ * functionality of the QueueRegistry, DAQSink, and DAQSource
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#include "appfwk/DAQSink.hpp"
+#include "appfwk/DAQSource.hpp"
+#include "appfwk/StdDeQueue.hpp"
+
+#include "ers/ers.h"
+
+#define BOOST_TEST_MODULE DAQSink_DAQSource_test // NOLINT
+
+#include <boost/test/unit_test.hpp>
+
+using namespace dunedaq::appfwk;
+
+BOOST_AUTO_TEST_SUITE(DAQSink_DAQSource_test)
+
+/**
+ * @brief Initializes the QueueRegistry
+ */
+struct DAQSinkDAQSourceTestFixture
+{
+  DAQSinkDAQSourceTestFixture() {}
+
+  void setup()
+  {
+    std::map<std::string, QueueConfig> queuemap = { { "dummy", { QueueConfig::queue_kind::kStdDeQueue, 100 } } };
+
+    QueueRegistry::get().configure(queuemap);
+  }
+};
+
+BOOST_TEST_GLOBAL_FIXTURE(DAQSinkDAQSourceTestFixture);
+
+BOOST_AUTO_TEST_CASE(Construct)
+{
+  DAQSink<std::string> sink("dummy");
+  DAQSource<std::string> source("dummy");
+
+  DAQSink<int>* badsink;
+
+  BOOST_REQUIRE_EXCEPTION(badsink = new DAQSink<int>("dummy"),
+                          dunedaq::appfwk::DAQSinkConstructionFailed,
+                          [&](dunedaq::appfwk::DAQSinkConstructionFailed) { return true; });
+
+
+  DAQSource<int>* badsource;
+
+  BOOST_REQUIRE_EXCEPTION(badsource = new DAQSource<int>("dummy"),
+                          dunedaq::appfwk::DAQSourceConstructionFailed,
+                          [&](dunedaq::appfwk::DAQSourceConstructionFailed) { return true; });
+
+}
+
+BOOST_AUTO_TEST_CASE(InitialConditions)
+{
+
+  DAQSink<std::string> sink("dummy");
+  DAQSource<std::string> source("dummy");
+
+  BOOST_REQUIRE(sink.can_push());
+  BOOST_REQUIRE(!source.can_pop());
+}
+
+BOOST_AUTO_TEST_CASE(DataFlow)
+{
+
+  DAQSink<std::string> sink("dummy");
+  DAQSource<std::string> source("dummy");
+
+  sink.push(std::move("hello"));
+  std::string res;
+  source.pop(res);
+  BOOST_REQUIRE_EQUAL(res, "hello");
+}
+
+BOOST_AUTO_TEST_CASE(Exceptions)
+{
+
+  DAQSink<std::string> sink("dummy");
+  DAQSource<std::string> source("dummy");
+  std::string res;
+
+  BOOST_REQUIRE(!source.can_pop());
+  BOOST_REQUIRE(!source.pop(res));
+
+  for (int ii = 0; ii < 100; ++ii) {
+    BOOST_REQUIRE(sink.can_push());
+    sink.push("aaAaaa");
+  }
+
+  BOOST_REQUIRE(!sink.can_push());
+  BOOST_REQUIRE_EXCEPTION(sink.push("bbBbbb"), std::runtime_error, [&](std::runtime_error) {
+    return true;
+  }); // TODO: Change to ERS Issue and improve check
+}
+
+BOOST_AUTO_TEST_SUITE_END()
