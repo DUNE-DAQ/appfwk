96a8a0a3d1648ec45609363f0c3fdbeff34a28b9
Philip Rodrigues <rodriges@fnal.gov>
2020-06-20 18:42:37 +0100

    install the cmake modules, and tidy up the Findcetlib code

--- cmake/Findcetlib.cmake
+++ cmake/Findcetlib.cmake
@@ -8,31 +8,41 @@ if(EXISTS $ENV{CETLIB_LIB})
  # UPS
   find_library(CETLIB NAMES libcetlib.so)
   find_library(CETLIB_EXCEPT NAMES libcetlib_except.so)
-
-  message(STATUS "cetlib is ${CETLIB}")
-  message(STATUS "cetlib_except is ${CETLIB_EXCEPT}")
-# Create imported target 
-add_library(cetlib_except::cetlib_except SHARED IMPORTED)
-
-set_target_properties(cetlib_except::cetlib_except PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "$ENV{CETLIB_EXCEPT_INC}"
-  # TODO: maybe we need to link against cetlib_except's dependencies too?
-  # INTERFACE_LINK_LIBRARIES "$"
-  IMPORTED_LOCATION "${CETLIB_EXCEPT}"
-)
-
-# Create imported target 
-add_library(cetlib::cetlib SHARED IMPORTED)
-
-set_target_properties(cetlib::cetlib PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "$ENV{CETLIB_INC}"
-  INTERFACE_LINK_LIBRARIES "cetlib_except::cetlib_except"
-  IMPORTED_LOCATION "${CETLIB}"
-)
-
 else()
-	# Spack
-	find_package(cetlib REQUIRED)
-	set(CETLIB cetlib)
-	set(CETLIB_EXCEPT cetlib_except)
+  # Spack
+  find_package(cetlib REQUIRED)
+  set(CETLIB cetlib)
+  set(CETLIB_EXCEPT cetlib_except)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(cetlib
+  FOUND_VAR cetlib_FOUND
+  REQUIRED_VARS
+    CETLIB
+    CETLIB_EXCEPT
+)
+
+if(cetlib_FOUND AND NOT TARGET cetlib_except::cetlib_except)
+  # Create imported target 
+  add_library(cetlib_except::cetlib_except SHARED IMPORTED)
+  
+  set_target_properties(cetlib_except::cetlib_except PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "$ENV{CETLIB_EXCEPT_INC}"
+    # TODO: maybe we need to link against cetlib_except's dependencies too?
+    # INTERFACE_LINK_LIBRARIES "$"
+    IMPORTED_LOCATION "${CETLIB_EXCEPT}"
+    )
+endif()
+
+if(cetlib_FOUND AND NOT TARGET cetlib::cetlib)
+  # Create imported target 
+  add_library(cetlib::cetlib SHARED IMPORTED)
+  
+  set_target_properties(cetlib::cetlib PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "$ENV{CETLIB_INC}"
+    INTERFACE_LINK_LIBRARIES "cetlib_except::cetlib_except"
+    IMPORTED_LOCATION "${CETLIB}"
+    )
+  
 endif()
