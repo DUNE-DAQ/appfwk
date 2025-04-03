5ebd21597b66c100b74d5ab7eb16de97ae123c02
Eric Flumerfelt <eflumerf@fnal.gov>
2025-04-03 10:48:02 -0500

    Update Doxygen documentation and formatting of DAQModule.hpp

--- include/appfwk/DAQModule.hpp
+++ include/appfwk/DAQModule.hpp
@@ -1,15 +1,12 @@
 /**
  * @file DAQModule.hpp DAQModule Class Interface
  *
- * The DAQModule interface defines the required functionality for all
- * DAQModules that use the Application Framework. DAQModules are defined as "a
- * set of code which performs a specific task". They are connected to one
- * another within a DAQ Application by Queue instances, in a graph defined by a
- * ModuleList.
+ * The DAQModule interface defines the required functionality for all DAQModules that use the Application Framework.
+ * DAQModules are defined as "a set of code which performs a specific task". DAQModules are hosted within Applications
+ * and communicate with one another via the `iomanager` messaging system.
  *
- * This interface is intended to define only absolutely necessary methods to be
- * able to support the many different tasks that DAQModules will be asked to
- * perform.
+ * This interface is intended to define only absolutely necessary methods to be able to support the many different tasks
+ * that DAQModules will be asked to perform.
  *
  * This is part of the DUNE DAQ Application Framework, copyright 2020.
  * Licensing/copyright details are in the COPYING file that you should have
@@ -179,38 +176,28 @@ class DAQModule
 public:
   using data_t = nlohmann::json;
 
-  /**
-   * @brief DAQModule Constructor
-   * @param name Name of the DAQModule
-   */
   explicit DAQModule(std::string name)
     : utilities::NamedObject(name)
   {
   }
 
-  /**
-   * @brief DAQModule destructor
-   */
   virtual ~DAQModule() noexcept = default;
 
   /**
-   * @brief      Initializes the module
+   * @brief Initializes the module
+   * @param mcfg Pointer to the ConfigurationManager instance used to retrieve configuration objects
    *
    * Initialisation of the module. Abstract method to be overridden by derived classes.
    */
-  virtual void init(std::shared_ptr<ConfigurationManager> /*mcfg*/) = 0;
+  virtual void init(std::shared_ptr<ConfigurationManager> mcfg) = 0;
 
   /**
    * @brief Execute a command in this DAQModule
-   * @param cmd The command from CCM
-   * @param args Arguments for the command from CCM
-   * @return String with detailed status of the command (future).
+   * @param name The command from CCM
+   * @param data Arguments for the command from CCM
    *
-   * execute_command is the single entry point for DAQProcess to pass CCM
-   * commands to DAQModules. The implementation of this function should route
-   * accepted commands to the appropriate functions within the DAQModule.
-   *  Non-accepted commands or failure should return an ERS exception
-   * indicating this result.
+   * execute_command is the single entry point for DAQModuleManager to pass CCM commands to DAQModules. Failure should
+   * throw an ERS exception indicating this result.
    */
   void execute_command(const std::string& name, const data_t& data = {});
 
@@ -220,8 +207,11 @@ public:
 
 protected:
   /**
-   * @brief Registers a mdoule command under the name `cmd`.
-   * Returns whether the command was inserted (false meaning that command `cmd` already exists)
+   * @brief Registers a mdoule command under the name `name`.
+   * @param name Name of the command to add to the commands list executable by execute_command
+   * @param f Callback function that executes the command logic within the DAQModule
+   *
+   * This method will throw a CommandRegistrationFailed ERS exception if the command could not be added
    */
   template<typename Child>
   void register_command(const std::string& name, void (Child::*f)(const data_t&));
@@ -237,11 +227,9 @@ private:
 };
 
 /**
- * @brief Load a DAQModule plugin and return a shared_ptr to the contained
- * DAQModule class
+ * @brief Load a DAQModule plugin and return a shared_ptr to the contained DAQModule class
  * @param plugin_name Name of the plugin, e.g. DebugLoggingDAQModule
- * @param instance_name Name of the returned DAQModule instance, e.g.
- * DebugLogger1
+ * @param instance_name Name of the returned DAQModule instance, e.g. DebugLogger1
  * @return shared_ptr to created DAQModule instance
  */
 inline std::shared_ptr<DAQModule>
